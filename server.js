49a5c45cf37e60b8ef6e51ed6087804dea348fb9
Meredith Bashert <merbashert@gmail.com>
2019-11-20 16:23:18 -0500

    installed dotenv

--- server.js
+++ server.js
@@ -2,16 +2,34 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
 
+
+const db = mongoose.connection
+require('dotenv').config()
+
+
+///////////////////////
+
+/////Port//////
+const PORT = process.env.PORT
+
+///////////////////////
+
+/////Database//////
+const MONGODB_URI = process.env.MONGODB_URI
+
+mongoose.connect(MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true
+});
+
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+///////////////////////
+
 app.use(express.json())
 app.use(express.static('public'))
 
-
-
-mongoose.connect('mongodb://localhost:27017/flightcrud', {useNewUrlParser: true, useUnifiedTopology: true});
-
-mongoose.connection.once('open', () =>{
-    console.log('connected to mongoose');
-})
 
 app.get('/flights', (req, res) => {
     res.send("Hello World")
