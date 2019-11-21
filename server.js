19d043b50468bf1dfdd6b66b5f18ae1a6de77682
Veronica Ramirez <55036002+VeroElaine@users.noreply.github.com>
2019-11-21 07:46:01 -0800

    pushing to dev

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const MONGODB_URI = process.env.MONGODB_URI
 
 mongoose.connect(MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true
 });
-
+console.log("line22");
 // Error / success
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
 db.on('connected', () => console.log('mongo connected: '));
