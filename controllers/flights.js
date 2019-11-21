d4ce3d622ab2c19ece661af4e2235511a393b8a5
Veronica Ramirez <55036002+VeroElaine@users.noreply.github.com>
2019-11-21 08:02:09 -0800

    created flights controller and flights model

---
+++ controllers/flights.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+const Flights = require("../models/flights.js");
+
+router.get("/", (req, res) => {
+    Flights.find({}, (error, foundFlights) => {
+        res.json(foundFlights);
+    })
+});
+
+router.post("/", (req, res) => {
+    Flights.create(req.body, (error, createdFlights) => {
+        res.json(createdFlight)
+    })
+});
+
+router.delete("/:id", (req, res) => {
+    Flights.findByIdAndDelete(req.params.id, (error, deletedFlight) => {
+        res.json(deletedFlight)
+    })
+});
+
+router.put("/:id", (req, res) => {
+    Flights.findByIdAndUpdate(req.params.id, req.body, {new:true}, (error, updatedFlight) => {
+        res.json(updatedFlight)
+    })
+});
+
+module.exports = router;
---
+++ models/flights.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const flightSchema = new mongoose.Schema({
+    country: {type:String, required:true},
+    currency: {type:Number, required:true},
+    locale: {type:String, required:true},
+    originPlace: {type:String, required:true},
+    destinationPlace:{type:String, required:true},
+    outboundPartialDate:{type:String, required:true},
+    inboundPartialDate:{type:String, required:true},
+});
+
+const Flights = mongoose.model("Flight", flightSchema);
+
+module.exports = Flights;
