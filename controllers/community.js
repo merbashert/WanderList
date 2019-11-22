3c60ef065d586fdf5bb32244bfcb4fdcabceac62
Meredith Bashert <merbashert@gmail.com>
2019-11-22 13:23:42 -0500

    community controller

--- controllers/community.js
+++ controllers/community.js
@@ -3,29 +3,26 @@ const router = express.Router();
 const Community = require("../models/community.js");
 
 router.get("/", (req, res) => {
-    if(req.session.username) {
-        Community.find({}, null, {sort: {destination: 1}}(error, foundCommunity) => {
-            res.json(foundCommunity);
-        })
-    }
-
+    Community.find({}, null, {sort: {destination: 1}}(error, foundCommunity) => {
+        res.json(foundCommunity);
+    })
 });
 
 router.post("/", (req, res) => {
-    Community.create(req.body, (error, createdFlight) => {
-        res.json(createdFlight)
+    Community.create(req.body, (error, createdCommunity) => {
+        res.json(createdCommunity)
     })
 });
 
 router.delete("/:id", (req, res) => {
-    Community.findByIdAndDelete(req.params.id, (error, deletedFlight) => {
-        res.json(deletedFlight)
+    Community.findByIdAndDelete(req.params.id, (error, deletedCommunity) => {
+        res.json(deletedCommunity)
     })
 });
 
 router.put("/:id", (req, res) => {
-    Community.findByIdAndUpdate(req.params.id, req.body, {new:true}, (error, updatedFlight) => {
-        res.json(updatedFlight)
+    Community.findByIdAndUpdate(req.params.id, req.body, {new:true}, (error, updatedCommunity) => {
+        res.json(updatedCommunity)
     })
 });
 
