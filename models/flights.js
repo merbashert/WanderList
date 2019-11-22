129a125518d43637b1614b5137388dcc90d12371
Kristina Zbinden <53029467+kristinazbinden@users.noreply.github.com>
2019-11-22 15:10:42 -0800

    Added ids unique to the user who created them to created flights

--- models/flights.js
+++ models/flights.js
@@ -8,6 +8,7 @@ const flightSchema = new mongoose.Schema({
     destinationPlace:{type:String, required:true},
     outboundPartialDate:{type:String, required:true},
     inboundPartialDate:{type:String, required:true},
+    userid: {type:String, required: false}
 });
 
 const Flights = mongoose.model("Flight", flightSchema);
