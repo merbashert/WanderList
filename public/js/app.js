cc82640755afadfd965e75685d640c4d8577d5cd
Kristina Zbinden <53029467+kristinazbinden@users.noreply.github.com>
2019-11-26 12:26:44 -0800

    Added styling

--- public/js/app.js
+++ public/js/app.js
@@ -232,20 +232,19 @@ app.controller("SearchFlightsController", ["$http", function($http){
     });
 }
 
-    this.currency = function() {
-      $http({
-          method: 'GET',
-          url: "https://currency-converter5.p.rapidapi.com/currency/historical/2018-02-09?format=json&to="+this.toCountry+"&from="+this.fromCountry+"&amount=1",
-          headers: {
-              'x-rapidapi-host': 'currency-converter5.p.rapidapi.com',
-              'x-rapidapi-key': '9fa6b2d0ccmsh9b3a7a2f6ec8326p199982jsn4ed4b0a8a532'
-          },
-      }).then(function(response){
-          console.log(response);
-          controller.foundCurrency = response.data
-          console.log(response.data.amount);
-          console.log(response.data);
-      });
+this.currency = function() {
+  $http({
+      method: 'GET',
+      url: "https://currency-converter5.p.rapidapi.com/currency/historical/2018-02-09?format=json&to="+this.toCountry+"&from="+this.fromCountry+"&amount=1",
+      headers: {
+          'x-rapidapi-host': 'currency-converter5.p.rapidapi.com',
+          'x-rapidapi-key': '9fa6b2d0ccmsh9b3a7a2f6ec8326p199982jsn4ed4b0a8a532'
+      },
+  }).then(function(response){
+      console.log(response);
+      controller.foundCurrency = response;
+      console.log(response.data.amount);
+      console.log(response.data);
+  });
 };
-
 }])
